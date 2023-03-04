e6412859dfd1f51a0feb0017902d6f7c7fdd81ac
DanAlbertSolis <dansolis9@gmail.com>
2023-03-04 00:08:53 -0800

    update

--- src/wordleSolver.java
+++ src/wordleSolver.java
@@ -18,14 +18,16 @@ import org.json.simple.parser.*;
 
 
 class wordleChecker {
-    List<List<String>> wordList = new ArrayList<>();
+    List<String> wordList = new ArrayList<>();
 
     Scanner stdin = new Scanner(System.in);
-    String initWord, result;
+    String word, result;
     List<Character> possibleLetters = new ArrayList<Character>();
     List<Character> badLetters = new ArrayList<Character>();
-    Character finalWord[] = {};
-    String[] sixLetterWords = new String[100];
+
+    List<Character> finalWord = new ArrayList<Character>();
+    boolean allGreen = false;
+
 
 
 
@@ -36,41 +38,120 @@ class wordleChecker {
 
     }
 
-    public wordleChecker(List<List<String>> listArg) {
-    wordList = listArg;
+    public wordleChecker(List<String> list) {
+    wordList = list;
     }
 
     public void initCheck(){
-        System.out.print("What word was initially entered?\n");
-        initWord = stdin.next();
-        System.out.print("\nEnter the results of the initial guess");
+        System.out.print("What word was entered? (Common starter word is Crane) \n");
+        word = stdin.next();
+
+        System.out.println("Successfully initialized word: " + word);
+
+        System.out.print("Enter the results of the initial guess in order from first letter to last letter (B = black (not used), G = green (correct spot), Y = yellow (used, but in the wrong spot), for example: BGYYB \n");
         result = stdin.next();
+        letterSorter(word,result);
+        checker();
     }
 
+    public void checker(){
+        System.out.print("What is the next word entered? \n");
+        word = stdin.next();
 
-    public void checker(String input){
-        for (int i = 0; i <= input.length(); i++){
-            if (input.charAt(i) == 'B' || input.charAt(i) == 'b'){
-                badLetters.add(input.charAt(i));
-            }
-            if (input.charAt(i) == 'Y' || input.charAt(i) == 'y'){
-                possibleLetters.add(input.charAt(i));
-            }
-            if (input.charAt(i) == 'G' || input.charAt(i) == 'g'){
-                finalWord[i] = input.charAt(i);
-            }
+        System.out.println("Successfully initialized word: " + word);
+
+        System.out.print("Enter the results of the initial guess in order from first letter to last letter (B = black (not used), G = green (correct spot), Y = yellow (used, but in the wrong spot), for example: BGYYB \n");
+        result = stdin.next();
+        letterSorter(word,result);
+
+        while(allGreen == false){
+            checker();
         }
     }
 
-    public void sorter(){
 
+
+    public void printArrays(){
+        System.out.println("The possible letters so far are: ");
+        for (int i = 0; i <= possibleLetters.size()- 1; i++){
+            System.out.print(possibleLetters.get(i)+" ");
+        }
+        System.out.println("");
+
+        System.out.println("The bad letters so far are: ");
+        for (int i = 0; i <= badLetters.size()- 1; i++){
+            System.out.print(badLetters.get(i)+" ");
+
+        }
+        System.out.println("");
+
+        System.out.println("The word we have so far is: ");
+        for (int i = 0; i <= finalWord.size() - 1; i++){
+            System.out.print(finalWord.get(i)+" ");
+        }
     }
 
-    public void displayWords(){
 
+    public void letterSorter(String input, String result){
+            for (int i = 0; i <= input.length() - 1 ; i++){
+            if (result.charAt(i) == 'B' || result.charAt(i) == 'b'){
+
+               /* for (int j = 0; j <= badLetters.size() - 1; j++){
+                    if (badLetters.get(j) == input.charAt(i)){
+                        System.out.println("Bad letter already exists: "+ input.charAt(i));
+                    }
+                    else if (j == badLetters.size() - 1 && badLetters.get(j) != input.charAt(i)){
+                        badLetters.add(input.charAt(i));
+                        System.out.println("Added bad letter: "+ input.charAt(i));
+                    }
+                } */
+
+                badLetters.add(input.charAt(i));
+                System.out.println("Added bad letter: "+ input.charAt(i));
+            }
+            else if (result.charAt(i) == 'Y' || result.charAt(i) == 'y'){
+                possibleLetters.add(input.charAt(i));
+                System.out.println("Added possible letter: "+ input.charAt(i));
+            }
+            else if (result.charAt(i) == 'G' || result.charAt(i) == 'g'){
+                finalWord.add(input.charAt(i));
+                System.out.println("Added letter "+ input.charAt(i) + " to the final word");
+            }
+        }
+        System.out.println("\nFinished looping through the results...");
+        System.out.println("Successfully added all good or bad letters");
+        printArrays();
     }
 
 
+    /**
+     * returns arrayList of possible words
+     * @return
+     */
+    public List<String> possibleWords(){
+        List<String> possibleWords = new ArrayList<>();
+
+        if (badLetters.size() >= 1){
+            for (int i = 0; i <= wordList.size() - 1; i++){
+                for (int j = 0; j <= wordList.get(i).length() - 1; j++){
+                    for (int k = 0; k <= badLetters.size() - 1; k++){
+                        if (wordList.get(i).charAt(j) == badLetters.get(k)){
+                            wordList.remove(i);
+                            System.out.println("Removed word " +wordList.get(i) +" because the letter " + wordList.get(i).charAt(j) + " exists " );
+                        }
+                    }
+                }
+            }
+        }
+
+        if (possibleLetters.size() >= 1){
+            for (int i = 0; i <= wordList)
+        }
+
+
+
+        return wordList;
+    }
 }
 
 public class wordleSolver{
@@ -80,12 +161,15 @@ public class wordleSolver{
         int i = 0;
 
         List<String> wordList = new ArrayList<String>();
-        
+
         try (BufferedReader br = new BufferedReader(new FileReader("sgb-words.txt"))) {
             while((line= br.readLine()) != null){
                 wordList.add(line);
-                System.out.println(wordList.get(i++));
+                //System.out.println(wordList.get(i++)); //used to test if the words are correctly inputted into the array
             }
+            wordleChecker wordle1 = new wordleChecker();
+            wordle1.initCheck();
+
 
         }
 
