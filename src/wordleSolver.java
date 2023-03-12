89c4442b8ac6289ed1c22997e2526ef0130d1515
Dan Solis MPB <dansolis9@gmail.com>
2023-03-11 17:06:32 -0800

    version 1.230311 (naming schema version-month-day of upload)

--- src/wordleSolver.java
+++ src/wordleSolver.java
@@ -63,6 +63,7 @@ class wordleChecker {
         System.out.print("Enter the results of the initial guess in order from first letter to last letter (B = black (not used), G = green (correct spot), Y = yellow (used, but in the wrong spot), for example: BGYYB \n");
         result = stdin.next();
         letterSorter(word, result);
+        wordRemover();
         System.out.println("Is the word finished? Y/N");
         String yn = stdin.next();
         if (yn.equals("Y") || yn.equals("y")){
@@ -107,7 +108,7 @@ class wordleChecker {
     public void letterSorter(String input, String result) {
         for (int i = 0; i <= result.length() - 1; i++) {
             if (result.charAt(i) == 'B' || result.charAt(i) == 'b') { //checks if letter is black
-                if (badLetters.contains(input.charAt(i))){
+                if (badLetters.contains(input.charAt(i)) || possibleLetters.contains(input.charAt(i))){
                     System.out.println("Letter " + input.charAt(i) + " skipped");
                 }
                 else {
@@ -128,32 +129,14 @@ class wordleChecker {
             else{
                 finWord[i] = input.charAt(i);
                 //finalWord.add(i, input.charAt(i));
-                dict.put(input.charAt(i),i);
-                System.out.println("Added letter " + input.charAt(i) + " to the final word");
+                dict.put(input.charAt(i),i);//key = char, value = index
+                System.out.println("Added letter " + input.charAt(i) + " to the final word at index " + i);
 
-                for (Map.Entry <Character, Integer> entry : dict.entrySet()){
-                    for (int a = wordList.size() - 1 ; a >= 0; a--){
-                        String s  = wordList.get(a);
-
-                        if (s.charAt(entry.getValue()) != entry.getKey()){
-                            wordList.remove(a);
-                        }
-
-
-                        /*for (int b = 0; b < s.length(); b++){ //for each words letter
-                            if (s.charAt(b) != entry.getKey()){
-                                wordList.remove(a);
-                                break;
-                            }
-                        }*/
-                    }
-                }
             }
         }
         System.out.println("\nFinished looping through the results...");
         System.out.println("Successfully added all good or bad letters");
         printStatus();
-        printPossibleWords();
     }
 
 
@@ -164,7 +147,7 @@ class wordleChecker {
                 String s  = wordList.get(j);
                 for (int k = 0; k < s.length(); k++) { //for each words letter
                     if (s.charAt(k) == badLetters.get(i)){
-                        //System.out.println("Removed word '" + wordList.get(j)+ "' from the list because the letter " + s.charAt(k) + " is at index " + k);
+                        System.out.println("Removed word '" + wordList.get(j)+ "' from the list because the letter " + s.charAt(k) + " is at index " + k + " and is equal to badLetter " + badLetters.get(i));
                         wordList.remove(j);
                         break;
                     }
@@ -172,10 +155,24 @@ class wordleChecker {
             }
         }
 
-        for (int j = wordList.size() - 1 ; j >= 0; j--){
-
+        for (Map.Entry <Character, Integer> entry : dict.entrySet()){
+            System.out.println("Map remover is entered 1");
+            for (int a = wordList.size() - 1 ; a >= 0; a--){
+                String s  = wordList.get(a);
+                if (s.charAt(entry.getValue()) != entry.getKey()){
+                    System.out.println("Map remover is entered 2, removed word " + wordList.get(a) + " because " + s.charAt(entry.getValue()) + " doesnt equal " + entry.getKey());
+                    wordList.remove(a);
+                }
+                        /*for (int b = 0; b < s.length(); b++){ //for each words letter
+                            if (s.charAt(b) != entry.getKey()){
+                                wordList.remove(a);
+                                break;
+                            }
+                        }*/
+            }
         }
-        System.out.println("There are " + wordList.size() + " words left in the list");
+
+        printPossibleWords();
 
     }
 }
